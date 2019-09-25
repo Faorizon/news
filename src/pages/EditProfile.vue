17470408df12bba3d93f37fc581d114ae57f4177
Faorizon <53572881+Faorizon@users.noreply.github.com>
2019-09-25 18:39:28 +0800

    密码修改编辑成功

--- src/pages/EditProfile.vue
+++ src/pages/EditProfile.vue
@@ -21,7 +21,18 @@
             <van-field :value="profile.nickname" placeholder="请输入用户名" ref="nickname"/>
       </van-dialog>
 
-      <CellBar label="密码" text="profile.password" type="password"/>
+      <CellBar label="密码" text="profile.password" type="password" @click="show2=!show2"/>
+      <!-- 编辑密码的输入框 -->
+      <van-dialog
+            v-model="show2"
+            title="编辑密码"
+            show-cancel-button
+            @confirm="handlePassword"
+            >
+            <!-- 读取value昵称 -->
+            <van-field :value="profile.password" placeholder="请输入密码" ref="password"/>
+      </van-dialog>
+
       <CellBar label="性别" :text="profile.gender===1 ? '男' : '女'"/>
   </div>
 </template>
@@ -36,6 +47,7 @@ export default {
             profile:{},
             //昵称弹窗
             show1:false,
+            show2:false,
         }
     },
     components:{
@@ -43,6 +55,30 @@ export default {
         CellBar
     },
     methods:{
+        //请求编辑资料的接口
+        //data要提交给接口的数据
+        editProfile(data,callback){
+             this.$axios({
+                url:'/user_update/'+localStorage.getItem('user_id'),
+                method:'post',
+                //添加头信息
+                headers:{
+                    Authorization:localStorage.getItem("token")
+                },
+                data
+            }).then(res=>{
+                const {message} = res.data;
+                //成功的弹框提示
+                if(message==='修改成功'){
+                    //传入回调函数
+                    //等于 callback && callback()
+                    if(callback){
+                        callback();
+                    }
+                    this.$toast.success(message);
+                }
+            })
+        },
         afterRead(file){
             //构造表单数据
             const formData=new FormData();
@@ -62,50 +98,24 @@ export default {
                 //替换用户头像
                 this.profile.head_img=this.$axios.defaults.baseURL+data.url
                 // console.log(this.profile.head_img)
-                this.$axios({
-                    url:'/user_update/'+localStorage.getItem('user_id'),
-                    method:'POST',
-                    //添加头信息
-                    headers:{
-                        Authorization:localStorage.getItem("token")
-                    },
-                    data:{
-                        head_img:data.url
-                    }
-                }).then(res=>{
-                    const {message} = res.data;
-                    //成功的弹框提示
-                    if(message==='修改成功'){
-                        this.$toast.success(message);
-                    }
-                })
+                this.editProfile({head_img:data.url})
             })
         },
-        handleNickname(){
-            
+
+        handleNickname(){  
             //获取input输入框的值
             const value=this.$refs.nickname.$refs.input.value;
             // console.log(value)
             //提交到编辑资料的接口
-            this.$axios({
-                url:'/user_update/'+localStorage.getItem('user_id'),
-                method:'post',
-                //添加头信息
-                headers:{
-                    Authorization:localStorage.getItem("token")
-                },
-                data:{
-                    nickname:value
-                }
-            }).then(res=>{
-                const {message} = res.data;
-                //成功的弹框提示
-                if(message==='修改成功'){
-                    //刷新或者说替换掉页面的昵称
-                    this.profile.nickname=value;
-                    
-                    this.$toast.success(message);
-                }
+            this.editProfile({nickname:value},()=>{
+                this.profile.nickname=value;
+            })
+        },
+        handlePassword(){
+            //获取input输入框的值
+            const value=this.$refs.password.$refs.input.value;
+            this.editProfile({nickname:value},()=>{
+                this.profile.password=value;
             })
         }
     },

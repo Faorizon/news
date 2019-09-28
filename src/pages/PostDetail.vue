4149c9b3b2c2bc3812342072f00ca3918fdd4cf1
Faorizon <53572881+Faorizon@users.noreply.github.com>
2019-09-28 18:35:59 +0800

    普通数据渲染

--- src/pages/PostDetail.vue
+++ src/pages/PostDetail.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="container">
         <!-- 文章详情页的内容 -->
         <div class="article">
             <div class="header">
@@ -7,15 +7,12 @@
                     <span class="iconfont iconjiantou2"></span>
                     <span class="iconfont iconnew"></span>
                 </div>
-                <span class="focus">关注</span>
+                <span class="focus" v-if="!detail.has_follow">关注</span>
+                <span class="focus focus_active" v-else>已关注</span>
             </div>
-            <h3>文章的详情页内容</h3>
-            <p class="post-info">火星时报 2019-10-10</p>
-            <div class="post-conten">
-                文章的详情页内容
-                文章的详情页内容
-                文章的详情页内容
-            </div>
+            <h3>{{detail.title}}</h3>
+            <p class="post-info">{{detail.user.nickname}} 2019-10-10</p>
+            <div class="post-conten" v-html="detail.content"></div>
         </div>
         <div class="post-btns">
             <span>
@@ -36,13 +33,46 @@
 <script>
 import PostFooter from "@/components/PostFooter"
 export default{
+    data(){
+        return{
+            //文章的详情
+            detail:{
+                //提前设置空对象，避免报错
+                user:{}
+            }
+        }
+    },
     components:{
         PostFooter
+    },
+    mounted(){
+        //请求文章的详情
+        const {id}=this.$route.params;
+
+        const token=localStorage.getItem("token")
+
+        //请求的配置
+        const config={
+            url:"/post/"+id
+        }
+        if(token){
+            config.headers={
+                Authorization:token
+            }
+        }
+        this.$axios(config).then(res=>{
+            const {data}=res.data
+            this.detail=data
+            console.log()
+        })
     }
 }
 </script>
 
 <style lang="less" scoped>
+    .container{
+        padding-bottom: 100/360*100vw;
+    }
     .article{
         padding: 0 20px;
         .header{
@@ -69,6 +99,11 @@ export default{
                 color: #fff;
                 border-radius: 50px;
             }
+            .focus_active{
+                border:1px #ccc solid;
+                color:#333;
+                background: none;
+            }
         }
         h3{
             margin-bottom: 5px;
@@ -77,6 +112,9 @@ export default{
             font-size: 12px;
             color:#999;
             margin-bottom: 15px;
+        }
+        .post-content{
+            line-height: 1.5;
         }
     }
     .post-btns{
